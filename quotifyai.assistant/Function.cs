383889cfc70f7526e790efc70ba0d0739c5d1434
Cristian Pérez Matturro <cristian.perez@elexon.co.uk>
2024-11-27 17:51:57 +0100

    add interaction between the lambda and dynamo

--- quotifyai.assistant/Function.cs
+++ quotifyai.assistant/Function.cs
@@ -1,3 +1,5 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -7,15 +9,53 @@ namespace quotifyai.assistant;
 
 public class Function
 {
-    
+    private const string _DefaultTableName = "quotes";
+    private const string _DefaultRegion = "eu-west-3";
+    private const string _DefaultEndpoint = "";
+
+    private static readonly AmazonDynamoDBClient _dynamoDbClient;
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(ILambdaContext context)
+    public async Task FunctionHandler(ILambdaContext context)
     {
-        return "Hello, World!";
+        string tableName = Environment.GetEnvironmentVariable("QUOTES_TABLE_NAME") ?? _DefaultTableName;
+        var table = Table.LoadTable(_dynamoDbClient, tableName);
+
+        // Insert an item into the DynamoDB table
+        var document = new Document
+        {
+            ["documentId"] = Guid.NewGuid().ToString(),
+            ["createdDate"] = DateTime.UtcNow.ToString("yyyy-MM-dd'T'HH:mm:ssZ"),
+            ["documentData"] = "example"
+        };
+
+        await table.PutItemAsync(document);
+    }
+
+    static Function()
+    {
+        string dynamoDbEndpoint = Environment.GetEnvironmentVariable("DYNAMODB_ENDPOINT") ?? _DefaultEndpoint;
+        string region = Environment.GetEnvironmentVariable("AWS_REGION") ?? _DefaultRegion; 
+
+        if (!string.IsNullOrEmpty(dynamoDbEndpoint))
+        {
+            _dynamoDbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region),
+                ServiceURL = dynamoDbEndpoint
+            });
+        }
+        else
+        {
+            _dynamoDbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(region)
+            });
+        }
     }
 }
