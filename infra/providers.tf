d92d6c6628d3890d127ad525726d6c0808eac8d2
Cristian Perez Matturro <crperez.informatica@gmail.com>
2024-12-19 11:48:43 +0100

    azd workflow

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,11 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>3.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstatequotifyai"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 
-  required_version = ">= 1.5.0"
 }
 
 provider "azurerm" {
