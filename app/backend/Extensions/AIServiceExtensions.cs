e79fcc03a5f12afe2a9871a1ff9b6acc7b498573
Cristian Perez Matturro <crperez.informatica@gmail.com>
2024-12-27 14:04:48 +0100

    adding semantic kernel

--- app/backend/Extensions/AIServiceExtensions.cs
+++ app/backend/Extensions/AIServiceExtensions.cs
@@ -7,15 +7,24 @@ internal static class AIServiceExtensions
 {
     internal static IServiceCollection AddAIServices(this IServiceCollection services)
     {
+        var kernelBuilder = services.AddKernel();
+        kernelBuilder.Services.AddLogging(configure => configure.AddConsole());
+        kernelBuilder.Services.AddLogging(configure => configure.SetMinimumLevel(LogLevel.Trace));
+
+        AddVectorStore(services);
+        return services;
+    }
+
+    private static void AddVectorStore(IServiceCollection services)
+    {
         var vectorStoreEndpoint = Environment.GetEnvironmentVariable("VectorStoreEndpoint") ?? throw new InvalidOperationException("Vector Store endpoint is not set.");
         var vectorStoreUseHttps = Environment.GetEnvironmentVariable("VectorStoreUseHttps") ?? throw new InvalidOperationException("Vector Store useHttps is not set.");
         var vectorStorePort = Environment.GetEnvironmentVariable("VectorStorePort") ?? throw new InvalidOperationException("Vector Store port is not set.");
-        
+
         int port = int.Parse(vectorStorePort);
         var useHttps = bool.Parse(vectorStoreUseHttps);
-        
+
         services.AddSingleton<QdrantClient>(sp => new QdrantClient(vectorStoreEndpoint, port, useHttps));
         services.AddQdrantVectorStore();
-        return services;
     }
 }
--- app/backend/GlobalUsings.cs
+++ app/backend/GlobalUsings.cs
@@ -1,12 +1,13 @@
 global using Azure.Identity;
 global using Azure.Storage.Blobs;
 global using Azure.Storage.Blobs.Models;
-global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.SemanticKernel;
 global using MinimalApi.Extensions;
 global using MinimalApi.Services;
 global using Shared.Models;
 global using PdfSharpCore.Pdf;
 global using PdfSharpCore.Pdf.IO;
+global using Qdrant.Client;
 global using HealthChecks.UI.Client;
